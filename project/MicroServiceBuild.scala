2f9ab3728c0fcf5fb4f05290613ee4ef298b7594
masahmad <masahmad7@gmail.com>
2021-08-11 14:13:07 +0100

    remove env config from app.conf and add test dependencies

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -19,8 +19,9 @@ private object AppDependencies {
     "net.codingwell"         %% "scala-guice"        % "4.2.6",
     "com.beachape"           %% "enumeratum"         % "1.6.1",
     "org.jsoup"              % "jsoup"               % "1.13.1" % "test",
-    "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.3" % "test, it",
-    "uk.gov.hmrc"            %% "hmrctest"           % "3.9.0-play-26" % "test, it",
+    "org.scalatestplus.play" %% "scalatestplus-play"       % "5.1.0"         % "test, it",
+    "org.scalatestplus"      %% "mockito-3-4"              % "3.2.9.0"       % "test, it",
+    "uk.gov.hmrc"            %% "service-integration-test" % "1.1.0-play-28" % "test, it",
     "org.pegdown"            % "pegdown"             % "1.6.0" % "test, it",
     "org.mockito"            % "mockito-core"        % "3.6.0" % "test, it",
     "com.github.tomakehurst" % "wiremock"            % "2.27.2" % "test,it"
